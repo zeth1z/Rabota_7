e2af832127c59d064d2919fb3a466e3def890429
zeth1z <golittleh1z@gmail.com>
2025-10-22 10:03:07 +0300

    Update Program.cs in public static

--- Работа 7/Program.cs
+++ Работа 7/Program.cs
@@ -1,24 +1,3 @@
-﻿using System;
-public class Program
-{
-    static void Main()
-    {
-        Console.WriteLine("Программа для нахождения периметра, полупериметра и площади треугольника по трём его сторонам");
-        try
-        {
-            Console.Write("Введите длину стороны a: ");
-            double a = double.Parse(Console.Readline());
-            Console.Write("Введите длину стороны b: ");
-            double b = double.Parse(Console.Readline());
-            Console.Write("Введите длину стороны c: ");
-            double c = double.Parse(Console.Readline());
-        }
-        catch (FormatException)
-        {
-            Console.WriteLine("Ошибка, введите числовые значения для сторон треугольника");
-        }
-    }
-
     public static double CalculateTriangleArea(double a, double b, double c)
     {
         try
@@ -43,4 +22,3 @@ public class Program
             throw new Exception($"Математическая ошибка: {ex.Message}");
         }
     }
-}
